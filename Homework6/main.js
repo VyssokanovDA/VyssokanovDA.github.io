ca8d6fa5a161fa8de4cb114638cbeb0800d1bbed
VyssokanovDA <Vyssokanov@mail.ru>
2018-03-11 04:37:51 +0300

    Working calc without digit check

--- Homework6/main.js
+++ Homework6/main.js
@@ -8,42 +8,71 @@ var clear = document.getElementById('clear');
 var entries = document.getElementById('entries');
 //Кнопка равно
 var calculate = document.getElementById('calculate');
-var operator = "";
+var operator = ""; //строковое значение операции
+var result; //результат
+var q = 0; //флаг повторной операции
+var o = ""; //флаг сброса текстового поля
 
 for (var i = 0; i < numbers.length; i++) {
 	numbers[i].addEventListener("click", function(event){
+		if (o) {
+			if (!(o == 1)) {q = 1;} 
+			entries.value = "";
+			o = "";
+		}
 		entries.value += event.target.textContent;
-		console.log(event.target.textContent)
+		console.log(event.target.textContent);		
 	})
 }
 for (var i = 0; i < operations.length; i++) {
-	operations[i].addEventListener("click", function(event){
-		if (!hasMathSymbol() && entries.value) {
-		entries.value += event.target.textContent;
-		operator = event.target.textContent;
+	operations[i].addEventListener("click", function(event){		
+		console.log(q);
+		if (q) {
+			calc(result);
+		}else{
+			result = +entries.value;
 		}
-		console.log(event.target.textContent);
+		entries.value = result;
+		operator = event.target.textContent;
+		o = operator;					
+		//console.log(event.target.textContent);		
 	})
 }
-
+	
 	clear.addEventListener("click", function(event){
 		entries.value = "";
+		q = 0;
+		o = "";
 		console.log(event.target.textContent)
 	});
-
-
-	entries.addEventListener("click", function(event){
-		console.log(event.target.textContent)
-	});
-
 
 	calculate.addEventListener("click", function(event){
 		console.log(event.target.textContent)
+		if (q) {
+			calc(result);
+		}else{
+			result = +entries.value;
+		}
+		entries.value = result;
+		q = 0;
+		o = 1;
 	});
 
-function hasMathSymbol(){
-	var val = entries.value;
-	if ((val.indexOf('+') + 1) || (val.indexOf('-') + 1) || (val.indexOf('*') + 1) || (val.indexOf('/') + 1))
-		return true;
-	return false;
+
+function calc(r){
+	switch(operator) {
+		case "+":
+		result = r + +entries.value;		
+		break;
+		case "-":
+		result = r - +entries.value;		
+		break;
+		case "*":
+		result = r * +entries.value;		
+		break;
+		case "/":
+		result = r / +entries.value;		
+		break;
+		default:
+	}
 }
